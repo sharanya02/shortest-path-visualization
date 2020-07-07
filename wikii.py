8a3ae25094ea7c0d1a39edb00b77fe8bd5e3fd4a
Sharanya Mukherjee <59661067+sharanya02@users.noreply.github.com>
2020-07-07 16:39:46 +0530

    Add files via upload

---
+++ wikii.py
@@ -0,0 +1,31 @@
+from pyvis.network import Network
+import networkx as nx
+import wikipediaapi
+import networkx as nx
+import json
+G = nx.Graph()
+wiki = wikipediaapi.Wikipedia('en')
+def sm_graph(page):
+    links = page.links
+    for title in sorted(links.keys()):
+        G.add_node(title, size=20, title='couple')
+        G.add_edge(page.title, title, weight=1)
+        link1= wiki.page(title).links
+        for t in sorted(link1.keys()):
+            G.add_edges_from([(title,t)])
+    return G
+start_page="Rushbrook"
+end_page="Shawl"
+
+G1=sm_graph(wiki.page(start_page))
+k = nx.shortest_path(G1, start_page, end_page, weight='weight')
+print(k)
+for g in G.nodes:
+    if g in k:
+        G.node[g]['color']="blue"
+        G.node[g]['size']= 30
+for i in range(0,len(k)-1):
+    G[k[i]][k[i+1]]['color'] = "red"
+    nt = Network("500px", "500px")
+nt.from_nx(G1)
+nt.show("nx.html")
